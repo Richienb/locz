9d7e9d414706ee95515219f39d5e383f4aa05d15
Richie Bendall <richiebendall@gmail.com>
2020-08-26 17:19:24 +1200

    Only import platform-specific methods once

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const commandExists = require("command-exists")
 
+const macos = require("./lib/platforms/macos")
+const windows = require("./lib/platforms/windows")
 const ipLocation = require("./lib/ip-location")
 
 const { platform } = process
@@ -9,11 +11,11 @@ const { platform } = process
 module.exports = async () => {
 	try {
 		if (platform === "darwin") {
-			return await require("./lib/platforms/macos")()
+			return await macos()
 		}
 
 		if (platform === "win32" && await commandExists("powershell")) {
-			return await require("./lib/platforms/windows")()
+			return await windows()
 		}
 	} catch (_) { }
 
